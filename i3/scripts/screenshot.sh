8ff92e8e2dbb5f7cbe2eaf6b7cab3ba4a94a21ad
Ben <bensongsyz@gmail.com>
2023-04-21 13:29:31 +0800

    update
    Changes to be committed:
    	modified:   alacritty/alacritty.yml
    	modified:   i3/scripts/screenshot.sh
    	modified:   nvim
    	modified:   variety/variety.conf

--- i3/scripts/screenshot.sh
+++ i3/scripts/screenshot.sh
@@ -5,48 +5,48 @@
 #source $BASEDIR/dunstity.sh
 
 
-reply_action () {
-    echo
-}
-forward_action () {
-    echo
-}
-handle_dismiss () {
-    echo
+raw_copy(){
+    xclip -selection clipboard -t image/png -i "$1" # && trash-put "$1"
 }
 
-reply_action(){
-    echo "$1"
-    #xclip -selection clipboard -t image/png -i "$1" #&& rm $1
-    xclip -selection clipboard -t text -i <<<"$(echo $1)" #&& rm $1
+path_copy(){
+    # help: xclip -selection clipboard -target TARGETS -o
+    #xclip -selection clipboard -t UTF8_STRING -o
+    echo "$1" | xclip -selection clipboard -target UTF8_STRING -i
+    #xclip -selection clipboard -t UTF8_STRING -o
 }
 
 
 main(){
     mode="$1"
+    mode=select
     case "$mode" in
         "select")
-            screenshot_name="$(scrot -q 100 -z -s '/home/ben/Pictures/screenshots/%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo $f')"
+            screenshot_name="$(scrot -q 100 -z -s  "$HOME"/Pictures/screenshots/'%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo -n $f')"
             ;;
         "border")
-            screenshot_name="$(scrot -q 100 -z -bs '/home/ben/Pictures/screenshots/%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo $f')"
-            forward_action
+            screenshot_name="$(scrot -q 100 -z -bs "$HOME"/Pictures/screenshots/'%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo -n $f')"
             ;;
         "default")
-            screenshot_name="$(scrot -q 100 -z '/home/ben/Pictures/screenshots/%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo $f')"
+            screenshot_name="$(scrot -q 100 -z     "$HOME"/Pictures/screenshots/'%Y-%m-%d_%H%M%S_$wx$h_scrot.png' -e 'echo -n $f')"
             ;;
     esac
 
-    ACTION=$(dunstify --timeout=60000 --action="default,Reply" --action="forwardAction,Forward" "scrot $mode" "screenshot has been saved as $screenshot_name.")
+    ACTION=$(dunstify --timeout=60000 --action="default,Copy Image's raw data" --action="PathCopy,Copy File Path" --action="Close,Do nothing" "scrot $mode" "screenshot has been saved as $screenshot_name.")
+    echo "$ACTION"
     case "$ACTION" in
-        "default")
-            reply_action "$screenshot_name"
-            ;;
-        "forwardAction")
-            forward_action
+        "default") # right click
+            raw_copy "$screenshot_name"
             ;;
         "2")
-            handle_dismiss
+            path_copy "$screenshot_name"
+            ;;
+        # from do_actions
+        "PathCopy")
+            path_copy "$screenshot_name"
+            ;;
+        "Close")
+            do_action
             ;;
     esac
 }
