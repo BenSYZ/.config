9e171a7dd5027d92fba950d62f6ef76bafbcceed
Ben <bensongsyz@gmail.com>
2023-09-09 02:06:56 +0800

    update i3blocks/network.sh

--- i3blocks/network.sh
+++ i3blocks/network.sh
@@ -1,46 +1,22 @@
 #!/bin/bash
-wifi_dev="wlp5s0"
-#wifi=$( ip addr | grep "wlp5s0" |grep -E -o "inet ([0-9]{1,3}[\.]){3}[0-9]{1,3}" |awk '{print $2 " "}' )
-#wifi=$(ip addr | grep "inet .* wlp5s0" |sed -n 's/\ *inet\ *\(\([0-9]\{1,3\}.\)\{3\}.[0-9]\{1,3\}\).*/\1/p')
-#wifi=$(ip addr | grep "inet .* wlp5s0" |sed -n 's/\ *inet\ *\([0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}\).*/\1/p')
-#wifi=$(ip addr | grep "inet .* wlp5s0" |sed -n 's/\ *inet\ *\(\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\).*/\1/p')
-wifi=$(ip addr show "$wifi_dev" |awk '$1=="inet" {print(substr($2, 1, index($2,"/")-1))}')
-#line=$( ip addr | grep "enp4s0" |grep -E -o "inet ([0-9]{1,3}[\.]){3}[0-9]{1,3}" |awk '{print $2 " "}' )
-#line=$(ip addr | grep "inet .* enp4s0" |sed -n 's/\ *inet\ *\(\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\).*/\1/p')
-line=""
+show_text=""
 for netcard in $(ip l show |sed -n 's/^[0-9]*: \([^:]*\):.*LOWER_UP>.*/\1/p' |grep -v lo);do
-    line_info="$netcard ""$(ip addr show "$netcard" |awk '$1=="inet" {print(substr($2, 1, index($2,"/")-1))}')"
-    if [ "$line_info" = "$netcard " ];then
+    line_info=""
+    ap="$(iwgetid "$netcard" | sed -n 's/.*ESSID:"\(.*\)"$/\1/p')"
+    ip_addr="$(ip addr show "$netcard" |awk '$1=="inet" {print(substr($2, 1, index($2,"/")-1))}')"
+    ip_addr="${ip_addr/#192.168/-}" # remove 192.168
+    [ -z "$ip_addr" ] && \
         continue
+
+    if [ -n "$ap" ];then
+        echo "wireless"         >&2
+        short_ap="$(echo "$ap" | sed -n 's/\(..\).*\(.\)$/\1..\2/p')"
+        signal_strength=$(awk 'NR==3 {print $3}' /proc/net/wireless)
+        line_info="  $netcard $ip_addr|${signal_strength%\.}%|$short_ap"
+    else
+        echo "line"             >&2
+        line_info="󰈁 $netcard $ip_addr"
     fi
-    line="${line:+$line }󰈁 $line_info"
+    show_text="${show_text:+$show_text│}$line_info"
 done
-
-# hostname -i
-# ip addr show dev wlp5s0
-
-# when have special meaning
-# sed {: \{
-# sed (: \(, no matter if it's a group
-# sed ?: \?
-# sed [: [
-
-
-if [ -n "$wifi" ]; then
-    signal_strength=$(awk 'NR==3 {print $3}' /proc/net/wireless)
-    #ap=$(iw dev wlp5s0 info |awk '$1 == "ssid" {print $2}' |sed -n 's/.*\(..\)/\1/p')
-    ap="$(iwgetid $wifi_dev | sed -n 's/.*ESSID:"\(.*\)"$/\1/p')"
-    short_ap="$(echo "$ap" | sed -n 's/\(..\).*\(..\)$/\1-\2/p')"
-    echo -n "  $wifi"
-    echo -n " (${signal_strength%\.}% .$short_ap)"
-
-fi
-
-if [ -n "$line" ] && [ -n "$wifi" ];then
-    echo -n " |"
-fi
-
-if [ -n "$line" ]; then
-    echo -n "$line"
-fi
-echo
+echo "$show_text"
