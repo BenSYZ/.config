953ec3deaf7a2ad738474fa07f56b5609842359e
Ben <bensongsyz@gmail.com>
2023-09-15 18:25:42 +0800

    beauty i3blocks

--- i3blocks/memory.sh
+++ i3blocks/memory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Mem=$(free -h  |awk 'NR==2 {print $7}' )
 Swap=$(free -h |awk 'NR==3 {print $4}' )
-echo "${Mem%i}"'|'"${Swap%i}" # remove last i
+echo "${Mem%i}"'│'"${Swap%i}" # remove last i
 
