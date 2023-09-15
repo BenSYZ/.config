88582a3af1879c8936a3a32bb78a02305bed3ccc
Ben <bensongsyz@gmail.com>
2023-09-15 19:22:45 +0800

    i3blocks beauty weather, network, date

--- i3blocks/network.sh
+++ i3blocks/network.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+wifi_label='<span color="#91e78b"></span>'
+cable_label='<span color="#91e78b">󰈁</span>'
 show_text=""
 for netcard in $(ip l show |sed -n 's/^[0-9]*: \([^:]*\):.*LOWER_UP>.*/\1/p' |grep -v lo);do
     line_info=""
@@ -12,10 +15,10 @@ for netcard in $(ip l show |sed -n 's/^[0-9]*: \([^:]*\):.*LOWER_UP>.*/\1/p' |gr
         echo "wireless"         >&2
         short_ap="$(echo "$ap" | sed -n 's/\(..\).*\(.\)$/\1..\2/p')"
         signal_strength=$(awk 'NR==3 {print $3}' /proc/net/wireless)
-        line_info="  $netcard $ip_addr|${signal_strength%\.}%|$short_ap"
+        line_info="$wifi_label $netcard $ip_addr|${signal_strength%\.}%|$short_ap"
     else
         echo "line"             >&2
-        line_info="󰈁 $netcard $ip_addr"
+        line_info="$cable_label $netcard $ip_addr"
     fi
     show_text="${show_text:+$show_text│}$line_info"
 done
--- i3blocks/weather.sh
+++ i3blocks/weather.sh
@@ -3,13 +3,15 @@
 POSITION=nanshan,shenzhen
 [ "$POSITION" = "ningbo,zhejiang" ] && stationid=58468
 [ "$POSITION" = "nanshan,shenzhen" ] && stationid=59493
-echo $(curl -s http://www.nmc.cn/rest/weather\?stationid\=$stationid\&_\=$(date +%s) |jq -r '.data.real.weather | "\(.temperature) \(.info)"' | awk '{ printf $1 "°C "} ;
-{if ($2=="晴") print "☀ ";};
-{if ($2~".云") print "⛅";};
-{if ($2=="阴") print "☁ ";};
-{if ($2~".雨") print "🌧 ";};
-{if ($2~".雪") print "❄ ";}; ')
-curl "http://wttr.in/${POSITION}_0qp_transparency=200.png" -o /tmp/.variety_wttr.png
+echo $(curl -s http://www.nmc.cn/rest/weather\?stationid\=$stationid\&_\=$(date +%s) |jq -r '.data.real.weather | "\(.temperature) \(.info)"' | awk '
+{if ($2=="晴") print "<span color=\"#ffb84d\">󰖨 </span>";};
+{if ($2~".云") print "<span color=\"#acc2d9\"> </span>";};
+{if ($2=="阴") print "<span color=\"#c7c9c9\">󰖐 </span>";};
+{if ($2~".雨") print "<span color=\"#81c3fc\">🌧</span>";};
+{if ($2~".雪") print "<span color=\"#d5e4f7\"> </span>";};
+{ printf $1 "°C "} ;
+    ')
+timeout 10 curl "http://wttr.in/${POSITION}_0qp_transparency=200.png" -o /tmp/.variety_wttr.png
 
 # url
 # https://weather.cma.cn/web/weather/58468.html
