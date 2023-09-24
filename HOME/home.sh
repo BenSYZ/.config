a7d70a05c27a881f4bbee98b0875c9df1dcecd0e
Ben <bensongsyz@gmail.com>
2023-09-24 10:28:36 +0800

    move zshrc to ~/.config/zsh

--- HOME/home.sh
+++ HOME/home.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
-link_files=(".bashrc" ".npmrc" ".xinitrc" ".Xresources" ".zshrc" ".condarc" ".zprofile")
+link_files=(".bashrc" ".npmrc" ".xinitrc" ".Xresources" ".condarc" ".zprofile")
 for f in $link_files;do
     echo $f
     ln -s "$HOME/.config/HOME/$f"  "$HOME"/
 done
+
+ln -s "$HOME/.config/zsh/zshrc"     "$HOME"/.zshrc
+ln -s "$HOME/.config/zsh/zprofile"  "$HOME"/.zprofile
