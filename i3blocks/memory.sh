f1812e6c6b4ea452ac3ec127ce2c647f855d0a70
Ben <bensongsyz@gmail.com>
2024-04-30 11:09:16 +0800

    i3blocks: memory.sh

--- i3blocks/memory.sh
+++ i3blocks/memory.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 Mem=$(free -h  |awk 'NR==2 {print $7}' )
 Swap=$(free -h |awk 'NR==3 {print $4}' )
+if [ "$Swap" = "0B" ];then
+    echo "${Mem%i}"
+    exit 0
+fi
 echo "${Mem%i}"'│'"${Swap%i}" # remove last i
 
